14c9f0e26b8c22a951cf4644e2d9cd0481f75a26
Aaron Deas <deas.aaron@gmail.com>
2023-05-26 20:47:28 -0400

    Added some overlays on the image on the home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,20 +16,22 @@
 <main class="flex flex-col items-center">
 	<div class="relative">
 		{#if ready}
-			<img src={Webdev} alt="Web Dev" class="object-cover" usemap="#image-map" />
+			<div id="my-image-overlay" class="area-overlay cursor-pointer" />
 			<map name="image-map">
 				<area
 					id="area1"
 					href="#"
 					alt="Cartoon web developer in action."
-					coords="100,75,509,325" 
+					coords="450,230,524,287"
 					shape="rect"
 					on:click={handleArea1Click}
 					on:mouseover={() => console.log('area1 mouseover')}
 					on:focus={() => console.log('area1 focus')}
 					tabindex="0"
+					class="clickable-area"
 				/>
 			</map>
+			<img src={Webdev} id="hero-webdev" alt="Web Dev" class="object-cover" usemap="#image-map" />
 		{/if}
 	</div>
 	<div class="component-container h-full w-1/2">
@@ -46,5 +48,18 @@
 <style>
 	.component-container {
 		margin-bottom: 50vh;
+	}
+	.area-overlay {
+		position: absolute;
+		top: 228px;
+		left: 455px;
+		width: 75px;
+		height: 59px;
+		border: 4px solid transparent;
+		transition: border-color 0.4s ease-in-out;
+		border-radius: 4px;
+	}
+	.area-overlay:hover {
+		border-color: #03030339;
 	}
 </style>
